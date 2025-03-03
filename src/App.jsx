256fa5df31c315aa8776b9f7b63e596a7bbb4790
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2025-03-03 17:33:04 +0300

    setup and navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./sections/Navbar";
+
+const App = () => {
+  return (
+    <div className="text-white">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
