03ceb51ca43dfdbde1d57338241f0a8f5151ff34
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2025-03-04 15:25:19 +0300

    bento grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
+import About from "./sections/About";
 
 const App = () => {
   return (
     <div className="text-white">
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 };
