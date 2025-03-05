f6fbc04344a08762f993b7aebc76cccb46ee7015
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2025-03-05 14:10:00 +0300

    remove border from hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
 
   const sizes = calculateSizes(isSmall, isMobile, isTablet);
   return (
-    <section className="min-h-screen border-2 border-blue-500 w-full flex flex-col relative">
+    <section className="min-h-screen  w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
           Hi, I am sepehr <span className="waving-hand">👋</span>
